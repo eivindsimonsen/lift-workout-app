e14d605c72de053f3eeee295c922d268734e323c
Eivind Simonsen <74544541+eivindsimonsen@users.noreply.github.com>
2025-08-18 16:23:23 +0200

    Whitespace push

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -86,7 +86,7 @@ define(['./workbox-9dc17825'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.n7j6j1849b"
+    "revision": "0.egljdpv2qsg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
