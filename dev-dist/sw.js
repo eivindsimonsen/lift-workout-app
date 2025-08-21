306d5f6dbcdecdc481131ea662fcf5be4f57ed3b
Eivind Simonsen <74544541+eivindsimonsen@users.noreply.github.com>
2025-08-21 07:50:39 +0200

    Fixed a scrolling issue

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -86,7 +86,7 @@ define(['./workbox-9dc17825'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.krmmf990dl"
+    "revision": "0.0bc4ig9qrf8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
