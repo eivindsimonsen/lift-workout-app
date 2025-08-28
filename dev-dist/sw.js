3781503100a17e45c8b0a3f8679cf804d2f381e5
Eivind Simonsen <74544541+eivindsimonsen@users.noreply.github.com>
2025-08-28 10:25:05 +0200

    Updated category Ids to be numbers

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -86,7 +86,7 @@ define(['./workbox-9dc17825'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1oahtiea9no"
+    "revision": "0.a4pu16g8888"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
