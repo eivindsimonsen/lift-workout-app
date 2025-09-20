ce4d15f9068b994c8988ca214d922b94ee746c45
Eivind Simonsen <74544541+eivindsimonsen@users.noreply.github.com>
2025-09-20 21:25:45 +0200

    Minor design fixes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -86,7 +86,7 @@ define(['./workbox-9dc17825'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hlcr4iq56qg"
+    "revision": "0.35a1snmb0f"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
